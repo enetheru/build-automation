59d091e73cf77cd1615315dcab59fcd3090aac07
Samuel Nicholas <nicholas.samuel@gmail.com>
2024-11-11 01:27:33 +1030

    updated scripts

--- godot-cpp/Darwin-build.sh
+++ godot-cpp/Darwin-build.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 set -Ee
-# for a fresh configure add FRESH=--fresh to the start of the script invocation
-#> FRESH=--fresh ./build.sh
+prev_dir=$(pwd)
 
 gitUrl=http://github.com/enetheru/godot-cpp.git
 gitBranch="modernise"
-
-prev_dir=$(pwd)
 
 echo 
 echo " == Build $target using Darwin =="
 thisScript="$(basename $0)"
 echo "  thisScript  = $thisScript"
+
+# Get the target root from this script location
 targetRoot=$( cd -- "$( dirname -- "$0}" )" &> /dev/null && pwd )
 echo "  targetRoot  = $targetRoot"
+cd $targetRoot
 
+# Set pattern variable from first argument
 if [ -n "$argv[1]" ]; then
     pattern="$argv[1]"
     echo "  pattern     = $pattern"
 fi
 
-cd $targetRoot
-
+# Get script count
 buildScripts=($(rg -u --files --max-depth 1 | \
     rg "Darwin.*sh" | \
     rg -v "$thisScript"))
 
 echo "  Script count: ${#buildScripts}"
+
+#Fail if no scripts
 if [ ${#buildScripts} -eq 0 ]; then
     echo
     echo "  ${RED}Error: No build scripts found${NC}"
@@ -34,11 +36,17 @@ if [ ${#buildScripts} -eq 0 ]; then
     exit 1
 fi
 
+# Print Scripts
 echo "  Scripts:"
 for script in $buildScripts; do
     echo "    $script"
 done
 
+# Make sure the log directories exist.
+mkdir -p $targetRoot/logs-raw
+mkdir -p $targetRoot/logs-clean
+
+# Process Scripts
 for script in $buildScripts; do
     config=${script%.*}
     traceLog=$targetRoot/logs-raw/${config}.txt
@@ -54,25 +62,14 @@ for script in $buildScripts; do
 
         if ! Fetch;   then echo "${RED}Error: Fetch Failure${NC}"  ; continue; fi
         if ! Prepare; then echo "${RED}Error: Prepare Failure${NC}"; continue; fi
-        if ! Build;   then echo "${RED}Error: Build Failure${NC}"  ; continue; fi
+        if ! Build;   then echo "${RED}Error: Build Failure${NC}" && yes  ; continue; fi
         if ! Test;    then echo "${RED}Error: Test Failure${NC}"   ; fi
         if ! Clean;   then echo "${RED}Error: Clean Failure${NC}"  ; fi
-    } 2&>1 | tee $traceLog
+    } 2>&1 | tee $traceLog
 
     matchPattern='(register_types|memory|libgdexample|libgodot-cpp)'
-    rg -M2048 $matchPattern $traceLog | sed -E 's/\s+/\n/g' `
+    rg -M2048 $matchPattern $traceLog | sed -E 's/\s+/\n/g' \
         | sed -E ':a;$!N;s/(-(MT|MF|o)|\/D)\n/\1 /;ta;P;D' > $cleanLog
 done
 
 cd $prev_dir
-
-# rg -u --files | rg "memory.*o(bj)?" | xargs rm
-# rg -u --files | rg "example.*o(bj)?" | xargs rm
-# 
-# HOST_TARGET_LIST=(macos-cmake-macos)
-# for HOST_TARGET in ${HOST_TARGET_LIST[@]}
-# do
-#     BUILD_SCRIPT=${GODOTCPP}/${HOST_TARGET}.sh
-#     TRACE_LOG=${GODOTCPP}/${HOST_TARGET}.txt
-#     $SHELL -xve $BUILD_SCRIPT 2>&1 | tee $TRACE_LOG
-# done
