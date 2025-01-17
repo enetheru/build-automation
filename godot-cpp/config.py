582f30ae789a19adc782b0fb371e7cacf2ffa5b0
Samuel Nicholas <nicholas.samuel@gmail.com>
2025-01-17 12:59:35 +1030

    update scripts

--- godot-cpp/config.py
+++ godot-cpp/config.py
@@ -28,10 +28,12 @@ if {fetch}:
         git_fetch( config )
         
 if {build}:
-    stats['build'] = {{'name':'build'}}
-    terminal_title(f"Build - {name}")
-    with Timer(container=stats['build']):
-        build_scons( config, build_vars=[f'target={build_target}'] )
+    for target in {build_targets}:
+        build_vars = {build_vars} + [f'target={{target}}']
+        stats['build'] = {{'name':'target'}}
+        terminal_title(f"Build - {name}")
+        with Timer(container=stats['build']):
+            build_scons( config, build_vars=build_vars )
 
 h3( 'build_config stats' )
 pp( stats, indent=4 )
@@ -44,6 +46,7 @@ from pprint import pp
 from actions import *
 
 name = config['name']
+if 
 
 build_root = Path(config['build_root'])
 
@@ -107,17 +110,59 @@ msbuild_extras = ['--', '/nologo', '/v:m', "/clp:'ShowCommandLine;ForceNoAlign'"
 # │  \ \/\/ /| | ' \/ _` / _ \ V  V (_-<                                       │
 # │   \_/\_/ |_|_||_\__,_\___/\_/\_//__/                                       │
 # ╰────────────────────────────────────────────────────────────────────────────╯
+# Build Targets
+#   - lib - 'template_release','template_debug','editor'
+#   - test - 'template_release','template_debug','editor'
+#
+# Build Tool
+#   - CMake
+#   - SCons
+#
+# - msvc
+#   - msvc
+#   - using clang-cl
+# - mingw32
+#   - clion builtin
+#   - mingw64
+#   - msys64/ucrt64
+#   - msys64/mingw32
+#   - msys64/mingw64
+# - clang
+#   - llvm
+#   - llvm-mingw
+#   - msys64/clang32
+#   - msys64/clang64
+#   - msys64/clangarm64
+# - android(clang)
+# - emscripten(clang)
+#
+# Option Variations
+#   - Thread/noThread
+#   - Profile/NoProfile
+#   - Precision single/double
+#   - Hot Re-Load ON/OFF
+#   - Exceptions ON/OFF
 
-#[=================================[ SCons ]=================================]
-for build_target in ['template_release','template_debug','editor']:
-    new_config = SimpleNamespace(**{
-        'name' : f'Windows.SCons.{build_target}',
-        'build_target':build_target,
-        'script': scons_script,
-        'shell':'msys2.clang64'
-    })
+#[=======================[ SCons Default with Profile ]=======================]
+new_config = SimpleNamespace(**{
+    'name' : f'w64.scons.msvc',
+    'shell':'pwsh',
+    'script': scons_script,
+    'build_dir':'test',
+    'build_vars':['build_profile="build_profile.json"'],
+    'build_targets':['template_release','template_debug','editor'],
+})
+project_config.build_configs[new_config.name] = new_config
 
-    project_config.build_configs[new_config.name] = new_config
+#[=================[ MSYS2.Clang SCons Default with Profile ]=================]
+# new_config = SimpleNamespace(**{
+#     'name' : f'w64.scons.clang64',
+#     'build_targets':['template_release','template_debug','editor'],
+#     'script': scons_script,
+#     'build_vars':[],
+#     'shell':'msys2.clang64'
+# })
+# project_config.build_configs[new_config.name] = new_config
 
 # ╒════════════════════════════════════════════════════════════════════════════╕
 # │                 ███    ███  ██████  ██████  ██ ██      ███████             │
--- share/env_commands.py
+++ share/env_commands.py
@@ -1,24 +1,59 @@
 from types import SimpleNamespace
 
-# MARK: Shell Commands
+# MARK: Shells
 # ╭────────────────────────────────────────────────────────────────────────────╮
 # │  ___ _        _ _    ___                              _                    │
 # │ / __| |_  ___| | |  / __|___ _ __  _ __  __ _ _ _  __| |___                │
 # │ \__ \ ' \/ -_) | | | (__/ _ \ '  \| '  \/ _` | ' \/ _` (_-<                │
 # │ |___/_||_\___|_|_|  \___\___/_|_|_|_|_|_\__,_|_||_\__,_/__/                │
 # ╰────────────────────────────────────────────────────────────────────────────╯
+"""
+Windows Host
+    Compiler Environments / Toolchains
+        - msvc
+        - mingw32
+            - clion builtin
+            - mingw64
+            - msys64/ucrt64
+            - msys64/mingw32
+            - msys64/mingw64
+        - clang
+            - llvm
+            - llvm-mingw
+            - msys64/clang32
+            - msys64/clang64
+            - msys64/clangarm64
+        - android(clang)
+        - emscripten(clang)
+Linux Host
+    - Compiler Environment / Toolchain
+MacOS Host
+    - Compiler Environment / Toolchain
+"""
 
 shells = {
+    # Default Windows
     'pwsh':['pwsh', '-Command' ],
+    # MSYS2 GCC
     'msys2.mingw32': ['C:/msys64/msys2_shell.cmd', '-mingw32', '-defterm', '-no-start', '-c' ],
     'msys2.mingw64': ['C:/msys64/msys2_shell.cmd', '-mingw64', '-defterm', '-no-start', '-c' ],
     'msys2.ucrt64': ['C:/msys64/msys2_shell.cmd', '-ucrt64', '-defterm', '-no-start', '-c' ],
+    # MSYS2 Clang
     'msys2.clang64': ['C:/msys64/msys2_shell.cmd', '-clang64', '-defterm', '-no-start', '-c' ],
-    'msys2.msys': ['C:/msys64/msys2_shell.cmd', '-msys', '-defterm', '-no-start', '-c' ],
-    'msys2.clangarm64': ['C:/msys64/msys2_shell.cmd', '-clangarm64', '-defterm', '-no-start', '-c' ]
+    'msys2.clangarm64': ['C:/msys64/msys2_shell.cmd', '-clangarm64', '-defterm', '-no-start', '-c' ],
+    # MSYS2 default, doesnt really have a utility, ust here for completion
+    'msys2': ['C:/msys64/msys2_shell.cmd', '-msys', '-defterm', '-no-start', '-c' ],
+
     # TODO Emscripten
 }
-
+# MARK: Preamble
+# ╭────────────────────────────────────────────────────────────────────────────╮
+# │  ___         _      _     ___                    _    _                    │
+# │ / __| __ _ _(_)_ __| |_  | _ \_ _ ___ __ _ _ __ | |__| |___                │
+# │ \__ \/ _| '_| | '_ \  _| |  _/ '_/ -_) _` | '  \| '_ \ / -_)               │
+# │ |___/\__|_| |_| .__/\__| |_| |_| \___\__,_|_|_|_|_.__/_\___|               │
+# │               |_|                                                          │
+# ╰────────────────────────────────────────────────────────────────────────────╯
 #[===============================[ PowerShell ]===============================]
 def pwsh_preamble( defs:dict, command:str ) -> str:
     mini_script = ''
@@ -26,9 +61,6 @@ def pwsh_preamble( defs:dict, command:str ) -> str:
         mini_script += f'${k}="{v}"\n'
     mini_script += command
     return mini_script
-
-def pwsh_command() -> list:
-    return ['pwsh', '-Command']
 
 #[=================================[ Python ]=================================]
 def python_preamble( config:SimpleNamespace ) -> str:
@@ -46,99 +78,3 @@ def python_preamble( config:SimpleNamespace ) -> str:
         mini_script += f'\t{repr(k)}:{repr(v)},\n'
     mini_script += '}\n'
     return mini_script
-
-# MARK: Script Processing
-# ╭────────────────────────────────────────────────────────────────────────────╮
-# │  ___         _      _     ___                       _                      │
-# │ / __| __ _ _(_)_ __| |_  | _ \_ _ ___  __ ___ _____(_)_ _  __ _            │
-# │ \__ \/ _| '_| | '_ \  _| |  _/ '_/ _ \/ _/ -_|_-<_-< | ' \/ _` |           │
-# │ |___/\__|_| |_| .__/\__| |_| |_| \___/\__\___/__/__/_|_||_\__, |           │
-# │               |_|                                         |___/            │
-# ╰────────────────────────────────────────────────────────────────────────────╯
-
-def gen_script( config:dict ):
-    match config['script_language']:
-        case 'python':
-            return python_process_script( config )
-
-def python_process_script( config:dict ):
-    config['script'].format( **config )
-
-def python_command() -> list:
-    return ['python', '-c']
-#[==============================[ msys2 / env ]==============================]
-
-"""
-Windows Host
-    Compiler Environments / Toolchains
-        - msvc
-        - mingw32
-            - clion builtin
-            - mingw64
-            - msys64/ucrt64
-            - msys64/mingw32
-            - msys64/mingw64
-        - clang
-            - llvm
-            - llvm-mingw
-            - msys64/clang32
-            - msys64/clang64
-            - msys64/clangarm64
-        - android(clang)
-        - emscripten(clang)
-"""
-
-def python_command( defs:dict, command:str ) -> list:
-    from pathlib import WindowsPath
-    mini_script = 'import sys\n'
-    mini_script += 'config = {\n'
-    for k, v in defs.items():
-        if isinstance( v, WindowsPath ):
-            mini_script += f'\t{repr(k)}:{repr(str(v))},\n'
-            continue
-        mini_script += f'\t{repr(k)}:{repr(v)},\n'
-    mini_script += '}\n'
-    mini_script += f'\nsys.path.append(config["root_dir"])\n'
-    mini_script += command
-    return ['python', '-c', mini_script]
-
-# TODO
-# - msys
-# - bash
-# - zsh
-
-"""
-Windows Host
-    Compiler Environments / Toolchains
-        - msvc
-        - mingw32
-            - clion builtin
-            - mingw64
-            - msys64/ucrt64
-            - msys64/mingw32
-            - msys64/mingw64
-        - clang
-            - llvm
-            - llvm-mingw
-            - msys64/clang32
-            - msys64/clang64
-            - msys64/clangarm64
-        - android(clang)
-        - emscripten(clang)
-"""
-def msys2_clang64( config:dict, command:str ) -> list:
-    env_command = ['C:/msys64/msys2_shell.cmd', '-clang64', '-defterm',
-                   '-no-start', f'-where {config['project_root']}', '-c' ]
-    from pathlib import WindowsPath
-    mini_script = 'import sys\n'
-    mini_script += 'config = {\n'
-    for k, v in config.items():
-        if isinstance( v, WindowsPath ):
-            mini_script += f'\t{repr(k)}:{repr(str(v))},\n'
-            continue
-        mini_script += f'\t{repr(k)}:{repr(v)},\n'
-    mini_script += '}\n'
-    mini_script += f'\nsys.path.append(config["root_dir"])\n'
-    mini_script += command
-    return ['python', '-c', mini_script]
-    return
